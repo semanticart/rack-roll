8ec9efc5635046621e0576a8ef8ae720506e282a
Jeffrey Chupp <jchupp@politics4all.com>
2008-12-06 20:09:42 -0600

    first commit

---
+++ rack_roll.rb
@@ -0,0 +1,15 @@
+module Rack
+  class RackRoll
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if rand(1000) == 0
+        [302, {'Location' => 'http://www.youtube.com/watch?v=Yu_moia-oVI'}, []]
+      else
+        @app.call(env)
+      end
+    end
+  end
+end
